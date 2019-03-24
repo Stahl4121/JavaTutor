0b007e88941815ee146b1b0406070337b7153376
SamDiehl <samdiehl97@gmail.com>
2019-03-24 17:42:51 -0400

    Created LessonWeb

---
+++ JavaTutor/JavaTutor/LessonWeb.swift
@@ -0,0 +1,30 @@
+//
+//  LessonWeb.swift
+//  JavaTutor
+//
+//  Created by Sam Diehl on 3/24/19.
+//  Copyright © 2019 Logan Stahl. All rights reserved.
+//
+
+import UIKit
+
+class LessonWeb: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
