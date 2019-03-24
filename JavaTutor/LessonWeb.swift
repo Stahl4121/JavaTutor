35d268bcbb5a6c826cd39feb8f19b96c648456b0
SamDiehl <samdiehl97@gmail.com>
2019-03-24 16:44:51 -0400

    Set up web view

---
+++ JavaTutor/LessonWeb.swift
@@ -0,0 +1,29 @@
+//
+//  LessonWeb.swift
+//  
+//
+//  Created by Sam Diehl on 3/24/19.
+//
+
+import UIKit
+
+class LessonWeb: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
