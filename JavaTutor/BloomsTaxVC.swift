7ec8cf87b62537cb4f6c256e30697fcf41632a21
Emily <dynadan22@gmail.com>
2019-05-05 15:05:13 -0400

    Made a VC to track bloom's tax levels

---
+++ JavaTutor/BloomsTaxVC.swift
@@ -0,0 +1,30 @@
+
+//
+//  BloomsTaxVC.swift
+//  
+//
+//  Created by Emily Wasylenko on 5/5/19.
+//
+
+import UIKit
+
+class BloomsTaxVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
