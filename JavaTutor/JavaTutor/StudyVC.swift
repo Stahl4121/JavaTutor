cf8073f0e76431990a7c0a9db6af936e62d49a74
Emily <dynadan22@gmail.com>
2019-05-12 17:30:31 -0400

    Almost fixed functionality

--- JavaTutor/JavaTutor/StudyVC.swift
+++ JavaTutor/JavaTutor/StudyVC.swift
@@ -17,7 +17,10 @@ class StudyVC: UIViewController {
     @IBOutlet weak var lblBrushTopic: UILabel!
     @IBOutlet weak var lblWelcomeBack: UILabel!
     
+    var continueIndex : Int = 0
     var lowScoreIndex : Int = 0
+    var brushUpIndex : Int = 0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +32,7 @@ class StudyVC: UIViewController {
     
     func viewLoadSetup() {
         
-        lowScoreIndex = findLowestScore()
+        lowScoreIndex = findLowestScoreIndex()
         lblContinueTopic.text = studentRepo.continueTopic
         lblImproveTopic.text = domainRepo.moduleNames[lowScoreIndex]
         lblWelcomeBack.text = "Welcome back, \(studentRepo.username)!"
@@ -46,7 +49,36 @@ class StudyVC: UIViewController {
         }
     }
     
-    func findLowestScore() -> Int {
+    
+    func findContinueIndex() -> Int {
+        if studentRepo.recentActivities.count > 0 {
+            let stringToMatch = studentRepo.recentActivities[0]
+            for (i, s) in domainRepo.moduleNames.enumerated() {
+                if s == stringToMatch {
+                    return i
+                }
+            }
+        }
+        return 0
+    }
+    
+    
+    func findBrushUpIndex() -> Int {
+        if studentRepo.recentActivities.count > 0 {
+            
+            let stringToMatch = studentRepo.recentActivities[studentRepo.recentActivities.count - 1]
+            
+            for (i, m) in domainRepo.moduleNames.enumerated() {
+                if m == stringToMatch {
+                    return i
+                }
+            }
+        }
+        return 0
+    }
+    
+    
+    func findLowestScoreIndex() -> Int {
         var minScore = 100.0
         var indexOfMin = 0
         
@@ -56,25 +88,53 @@ class StudyVC: UIViewController {
                 indexOfMin = i
             }
         }
-        
         return indexOfMin
     }
     
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        lowScoreIndex = findLowestScore()
-        
-        if let child = segue.destination as? LessonListTVC {
-            child.module = lowScoreIndex
-            
-            //Update recent activities list
-            if let index = studentRepo.recentActivities.firstIndex(of: domainRepo.moduleNames[lowScoreIndex]){
-                studentRepo.recentActivities.remove(at: index)
+        continueIndex = findContinueIndex()
+        brushUpIndex = findBrushUpIndex()
+        lowScoreIndex = findLowestScoreIndex()
+     
+        if segue.identifier == "continue" {
+            if let child = segue.destination as? LessonListTVC {
+                if studentRepo.recentActivities.count > 0 {
+                    child.module = continueIndex
+                    updateRecentActivities(sentIndex: continueIndex)
+                } else {
+                    child.module = 0
+                    updateRecentActivities(sentIndex: 0)
+                }
             }
-            studentRepo.recentActivities.insert(domainRepo.moduleNames[lowScoreIndex], at: 0)
+        } else if segue.identifier == "brush" {
+            if let child = segue.destination as? LessonListTVC {
+                if studentRepo.recentActivities.count > 0 {
+                    child.module = brushUpIndex
+                    updateRecentActivities(sentIndex: brushUpIndex)
+                } else {
+                    child.module = 0
+                    updateRecentActivities(sentIndex: 0)
+                }
+            }
+        } else if segue.identifier == "improvement" {
+            if let child = segue.destination as? LessonListTVC {
+                child.module = lowScoreIndex
+                updateRecentActivities(sentIndex: lowScoreIndex)
+            }
         }
     }
 
+    /*
+     * Update recent activities list
+     */
+    func updateRecentActivities(sentIndex: Int){
+        if let index = studentRepo.recentActivities.firstIndex(of: domainRepo.moduleNames[sentIndex]){
+            studentRepo.recentActivities.remove(at: index)
+        }
+        studentRepo.recentActivities.insert(domainRepo.moduleNames[sentIndex], at: 0)
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         viewLoadSetup()
